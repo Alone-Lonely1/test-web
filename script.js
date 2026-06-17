0ccf771d0c68d517afe89d4d2fcc2af0df88b8d7
Alone-Lonely1 <japanesstk@gmail.com>
2026-06-17 14:08:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+    const startBtn = document.getElementById("startBtn");
+
+    if (startBtn) {
+
+        startBtn.addEventListener("click", function () {
+
+            window.location.href = "pages/intro.html";
+
+        });
+
+    }
+
+};
